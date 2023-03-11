8e6ba43ec94a2920f239f252d069defbec143ac2
AdarshKhedekar <98964225+AdarshKhedekar@users.noreply.github.com>
2023-03-11 20:52:27 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const multer = require("multer");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const File = require("./File.js");
+const mongoose = require("mongoose");
+dotenv.config();
+
+
+const url = 'mongodb+srv://adarsh:adarsh123@cluster0.clqwr0k.mongodb.net/filesharing?retryWrites=true&w=majority'
+
+// const url = process.env.DATABASE_URL;
+mongoose.connect(url).then(() => {
+  console.log('connection Successfull');
+}).catch((err) => {
+  console.log(err);
+})
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const storage = multer.diskStorage({
+  destination: "uploads/",
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
+
+app.get("/", (req, res) => {
+  res.send("eh");
+});
+app.post("/", upload.array("files", 10), async (req, res) => {
+  const files = req.files;
+  if (!files) {
+    return res.status(400).send("No file uploaded");
+  }
+  // console.log(files);
+  let fileUrls = [];
+  await Promise.all(
+    files.map(async (file) => {
+      const fileData = {
+        path: file.path,
+        originalName: file.originalname,
+      };
+      let f = await File.create(fileData);
+      fileUrls.push(`${req.protocol}://${req.get("host")}/uploads/${f.id}`);
+    })
+  );
+
+  res.json({ urls: fileUrls });
+});
+
+app.get("/uploads/:id", async (req, res) => {
+  const newFile = await File.findById(req.params.id);
+  res.download(newFile.path, newFile.originalName);
+});
+
+app.listen(process.env.PORT, (req, res) => {
+  console.log(`server listening on port ${process.env.PORT}`);
+});
